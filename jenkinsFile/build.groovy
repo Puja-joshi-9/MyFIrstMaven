c661174dbda458e4d4c9c22290b0a93237b56999
Puja Bhatt <pujabhatt@Pujas-MacBook-Air.local>
2021-10-01 13:55:28 -0400

    Jenkins File Added

---
+++ jenkinsFile/build.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    stages{
+        stage("checkout"){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Puja-joshi-9/MyFirstMaven.git']]])
+
+            }
+        }
+         stage("build"){
+            steps{
+                sh """
+                ls
+                """
+                
+            }
+        }
+    }
+    
+}
